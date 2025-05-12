9d671b35e1d392318d510631d64cd87cc2fa67c0
Marcus Davis <marcusmarcell@gmail.com>
2025-05-11 20:36:12 -0500

    LandPage Correct2

---
+++ landing-page-enhancements.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// This is a placeholder component that redirects to the ImaginationGLanding component
+// The actual implementation is now in ImaginationGLanding.tsx
+const LandingPageEnhancements: React.FC = () => {
+  return <div>Placeholder - see ImaginationGLanding.tsx for the actual implementation</div>;
+};
+
+export default LandingPageEnhancements;
