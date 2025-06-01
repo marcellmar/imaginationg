4d8dd1edd25558d27100191ed6fee07097fcb5a3
Marcus Davis <marcusmarcell@gmail.com>
2025-06-01 17:29:02 -0500

    Google Tags Implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { GoogleAnalytics } from '@next/third-parties/google';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-
-// Replace with your actual GA4 measurement ID
-const GA_MEASUREMENT_ID = 'G-V3R0S40J79';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -25,8 +21,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         
         const isAIReferral = aiDomains.some(domain => referrer.includes(domain));
         
-        if (isAIReferral && typeof window.gtag !== 'undefined') {
-          window.gtag('event', 'ai_referral', {
+        if (isAIReferral && typeof (window as any).gtag !== 'undefined') {
+          (window as any).gtag('event', 'ai_referral', {
             source: referrer,
             landing_page: router.pathname,
             timestamp: new Date().toISOString()
@@ -38,12 +34,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     trackAIReferral();
   }, [router.pathname]);
 
-  return (
-    <>
-      <Component {...pageProps} />
-      <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,18 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V3R0S40J79"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-V3R0S40J79');
+            `,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
